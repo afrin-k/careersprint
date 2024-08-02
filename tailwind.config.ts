955f91d64cb68f5563a68b0f832818ce0b60bcf5
afrin-k <afrinkhanam318@gmail.com>
2024-08-02 19:00:54 +0530

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+const defaultTheme=require('tailwindcss/defaultTheme');
 
 const config: Config = {
   content: [
@@ -8,13 +9,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      fontFamily: {
+        montserrat:['Montserrat','sans-serif',defaultTheme.fontFamily.sans],
+        ptserif:['PT_Serif','serif'],
+      }
     },
   },
   plugins: [],
 };
+
 export default config;
