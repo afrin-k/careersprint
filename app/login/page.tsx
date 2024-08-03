cf1c0a60ac2ea590bc03a7ef7a33286f687272cb
yashxita <gouriyashita@gmail.com>
2024-08-03 21:46:59 +0530

    login and signin modified

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,7 @@
-"use client"
-import Link from "next/link";
+"use client";
 
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import {
   CardTitle,
   CardDescription,
@@ -9,56 +10,67 @@ import {
   CardFooter,
   Card,
 } from "@/components/ui/card";
-import { useRouter } from "next/navigation";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 
 export default function Login() {
-  const router =useRouter();
-  const handleSubmitSignup=()=>{
-    void router.push("http://localhost:3000/login")
-  }
+  const router = useRouter();
+  const handleSubmitSignup = () => {
+    void router.push("http://localhost:3000/signup");
+  };
+
   return (
-    <div className="w-full  max-w-md items-center justify-center">
-      <form className="">
-        <Card>
-          <CardHeader className="space-y-1">
-            <CardTitle className="text-3xl font-bold">Login</CardTitle>
-            <CardDescription>
-              Enter your details to login to your account
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-4">
-            <div className="space-y-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="identifier"
-                name="identifier"
-                type="text"
-                placeholder="username or email"
-              />
+    <div className="flex items-center justify-center min-h-screen ">
+      <div className="w-full max-w-4xl shadow-2xl shadow-black rounded-lg">
+        <Card className="flex">
+        
+          <div className="w-1/2 ml-4">
+            <img
+              src="login.svg" 
+              alt="Login Image"
+              className="w-full h-full object-cover"
+            />
+          </div>
+         
+          <div className="w-1/2 p-8">
+            <CardHeader className="space-y-1">
+              <CardTitle className="text-3xl font-bold">Login</CardTitle>
+              <CardDescription>
+                Enter your details to login to your account
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-4">
+              <div className="space-y-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="identifier"
+                  name="identifier"
+                  type="text"
+                  placeholder="username or email"
+                />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder="password"
+                />
+              </div>
+            </CardContent>
+            <CardFooter className="flex flex-col">
+              <button className="w-full">Login</button>
+            </CardFooter>
+            <div className="mt-4 text-center text-sm">
+              Don't have an account?
+              <Link className="underline ml-2" onClick={handleSubmitSignup} href="signup">
+                Sign Up
+              </Link>
             </div>
-            <div className="space-y-2">
-              <Label htmlFor="password">Password</Label>
-              <Input
-                id="password"
-                name="password"
-                type="password"
-                placeholder="password"
-              />
-            </div>
-          </CardContent>
-          <CardFooter className="flex flex-col">
-            <button className="w-full">Login</button>
-          </CardFooter>
+          </div>
         </Card>
-        <div className="mt-4 text-center text-sm">
-          Don't have an account?
-          <Link className="underline ml-2" onClick={handleSubmitSignup} href="signup">
-            Sign Up
-          </Link>
-        </div>
-      </form>
+      </div>
     </div>
   );
 }
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-
+import { useRouter } from "next/navigation";
 import {
   CardTitle,
   CardDescription,
@@ -10,67 +10,78 @@ import {
   CardFooter,
   Card,
 } from "@/components/ui/card";
-
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
-import { useRouter } from "next/navigation";
 
 export default function Signup() {
   const router = useRouter();
-  const handleSubmitLogin=()=>{
-    void router.push("http://localhost:3000/login")
-  }
-  return (
-    <div className="w-full h-full max-w-md">
-      <form>
-        <Card>
-          <CardHeader className="space-y-1">
-            <CardTitle className="text-3xl font-bold">Sign Up</CardTitle>
-            <CardDescription>
-              Enter your details to create a new account
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-4">
-            <div className="space-y-2">
-              <Label htmlFor="username">Username</Label>
-              <Input
-                id="username"
-                name="username"
-                type="text"
-                placeholder="username"
-              />
-            </div>
-            <div className="space-y-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                name="email"
-                type="email"
-                placeholder="name@example.com"
-              />
-            </div>
+  const handleSubmitLogin = () => {
+    void router.push("http://localhost:3000/login");
+  };
 
-            <div className="space-y-2">
-              <Label htmlFor="password">Password</Label>
-              <Input
-                id="password"
-                name="password"
-                type="password"
-                placeholder="password"
-              />
+  return (
+    <div className="flex items-center justify-center min-h-screen ">
+      <div className="w-full max-w-5xl shadow-2xl shadow-black rounded-lg">
+        
+          
+          <Card className="flex">
+          <div className="w-1/2 ml-4">
+            <img
+              src="login.svg" 
+              alt="Login Image"
+              className="w-full h-full object-cover"
+            />
+          </div>
+          <div className="w-1/2 p-8">
+            <CardHeader className="space-y-1">
+              <CardTitle className="text-3xl font-bold">Sign Up</CardTitle>
+              <CardDescription>
+                Enter your details to create a new account
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-4">
+              <div className="space-y-2">
+                <Label htmlFor="username">Username</Label>
+                <Input
+                  id="username"
+                  name="username"
+                  type="text"
+                  placeholder="username"
+                />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="name@example.com"
+                />
+              </div>
+
+              <div className="space-y-2">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder="password"
+                />
+              </div>
+            </CardContent>
+            <CardFooter className="flex flex-col">
+              <button className="w-full">Sign Up</button>
+              <div className="mt-4 text-center text-sm">
+            Have an account?
+            <Link className="underline ml-2" onClick={handleSubmitLogin} href="login">
+              Login
+            </Link>
+          </div>
+            </CardFooter>
             </div>
-          </CardContent>
-          <CardFooter className="flex flex-col">
-            <button className="w-full">Sign Up</button>
-          </CardFooter>
-        </Card>
-        <div className="mt-4 text-center text-sm">
-          Have an account?
-          <Link className="underline ml-2" onClick={handleSubmitLogin} href="login">
-            Login
-          </Link>
-        </div>
-      </form>
+            </Card>
+      </div>
+      
     </div>
   );
 }
